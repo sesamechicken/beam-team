b0b910e75f00aa322a824025af4ccb91947b87d5
chris <chrisdickow@gmail.com>
2020-12-30 21:33:33 -0500

    Added drawer menu with icons

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,34 @@
 import React, { useState } from 'react';
 import { Route, Switch, withRouter, Link } from 'react-router-dom';
-import { Button, Drawer, List, ListItem , ListItemText} from '@material-ui/core';
+import { Button, Drawer, List, ListItem, ListItemIcon, ListItemText, IconButton, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import HomeIcon from '@material-ui/icons/Home';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import Home from '../components/home';
 import Roster from '../components/roster';
 import Profile from '../components/profile';
-import Header from '../components/header';
 import ItemShop from '../components/itemshop';
+import logo from '../static/logo.png';
 
 const useStyles = makeStyles((theme) => ({
   drawer: {
     width: 250,
     maxWidth: 360,
     backgroundColor: theme.palette.background.paper,
+  },
+  menuBttn: {
+    backgroundColor: '#fff'
+  },
+  root: {
+    flexGrow: 1,
+  },
+  logo: {
+    textAlign: 'center',
+    '& img': {
+      maxHeight: '80px'
+    }
   },
 }));
 
@@ -22,23 +38,41 @@ const Routes = (props) => {
 
   return (
     <div>
-      <p onClick={() => toggleDrawer(true)}>menu</p>
-      <div >
-        <Drawer elevation={10} anchor='left' open={drawerOpen} onClose={() => toggleDrawer(false)}>
-          <List className={classes.drawer}>
-            <ListItem button component={Link} to='/' onClick={() => toggleDrawer(false)}>
-              <ListItemText>Home</ListItemText>
-            </ListItem>
-            <ListItem button component={Link} to='/members' onClick={() => toggleDrawer(false)}>
-              <ListItemText>Members</ListItemText>
-            </ListItem>
-            <ListItem button component={Link} to='/itemshop' onClick={() => toggleDrawer(false)}>
-              <ListItemText>Item Shop</ListItemText>
-            </ListItem>
-          </List>
-      </Drawer>
+      <div className={classes.root}>
+        <Grid container spacing={3} >
+          <Grid item xs>
+            <IconButton className={classes.menuBttn} color="primary" aria-label="menu" onClick={() => toggleDrawer(true)}>
+              <MenuIcon />
+            </IconButton>
+          </Grid>
+          <Grid item xs className={classes.logo}>
+            <img src={logo} />
+          </Grid>
+          <Grid item xs></Grid>
+        </Grid>
       </div>
-      <Header />
+      <Drawer elevation={10} anchor='left' open={drawerOpen} onClose={() => toggleDrawer(false)}>
+        <List className={classes.drawer}>
+          <ListItem button component={Link} to='/' onClick={() => toggleDrawer(false)}>
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItem>
+          <ListItem button component={Link} to='/members' onClick={() => toggleDrawer(false)}>
+            <ListItemIcon>
+              <PeopleAltIcon />
+            </ListItemIcon>
+            <ListItemText primary="Members" />
+          </ListItem>
+          <ListItem button component={Link} to='/itemshop' onClick={() => toggleDrawer(false)}>
+            <ListItemIcon>
+              <ShoppingCartIcon />
+            </ListItemIcon>
+            <ListItemText primary="Item Shop" />
+          </ListItem>
+        </List>
+      </Drawer>
       <Switch>
         <Route exact path='/' render={(props) => <Home {...props}/>} />
         <Route exact path='/itemshop' render={(props) => <ItemShop {...props} />} />
